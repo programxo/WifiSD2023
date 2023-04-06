9257daf11c5fcaf6e893c5216a09a8b771ef2129
User <User@DEV-VM>
2023-04-06 19:09:58 +0200

    Using Scrutor to automatically register your services with the ASP.NET Core DI container (with ServiceBuilderExtension)

---
+++ SD.Persistence/Extensions/ServiceBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Design.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Wifi.SD.Core.Attributes;
+
+namespace SD.Persistence.Extensions
+{
+    public static class ServiceBuilderExtension
+    {
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.Scan(scan => scan
+            .FromAssemblies(Assembly.GetExecutingAssembly())
+            .AddClasses(c => c.WithAttribute<MapServiceDependencyAttribute>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            );
+        }
+    }
+}
